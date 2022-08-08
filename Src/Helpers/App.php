e57dc76927d0c59a3e2c3d8ce5fb5a671fa29245
s3minhtruong <minh.truong@s3corp.com.vn>
2022-08-08 10:17:10 +0700

    Create Application Helper Class

---
+++ Src/Helpers/App.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+
+use JetBrains\PhpStorm\Pure;
+use DateTimeInterface, DateTime, DateTimeZone;
+use mysql_xdevapi\Exception;
+
+class App
+{
+    private array $config = [];
+
+    public function __construct()
+    {
+        $this->config = Config::getFileContent('app');
+    }
+
+    public function isDebugMode(): bool
+    {
+        if (!isset($this->config['debug'])) {
+            return false;
+        }
+
+        return $this->config['debug'];
+    }
+
+    public function getEnvironment(): string
+    {
+        if (!isset($this->config['env'])) {
+            return 'production';
+        }
+
+        return $this->config['env'];
+    }
+
+    public function getLogPath(): string
+    {
+        if (!isset($this->config['log_path'])) {
+            throw new \Exception('Log path is not defined');
+        }
+
+        return $this->config['log_path'];
+    }
+
+    #[Pure] public function isRunningFromConsole(): bool
+    {
+        /**
+         * Returns the type of interface between web server and PHP
+         * @link https://php.net/manual/en/function.php-sapi-name.php
+         * @return string the interface type, as a lowercase string.
+         * </p>
+         * <p>
+         * Although not exhaustive, the possible return values include
+         * aolserver, apache,
+         * apache2filter, apache2handler,
+         * caudium, cgi (until PHP 5.3),
+         * cgi-fcgi, cli,
+         * continuity, embed,
+         * isapi, litespeed,
+         * milter, nsapi,
+         * phttpd, pi3web, roxen,
+         * thttpd, tux, and webjames.
+         */
+        return php_sapi_name() == 'cli' || php_sapi_name() == 'phpdbg';
+    }
+
+    public function getServerTime(): DateTimeInterface
+    {
+        try {
+            return new DateTime('now', new DateTimeZone('ASIA/Ho_Chi_Minh'));
+        } catch (\Throwable $exception) {
+            throw new \RuntimeException(
+                sprintf('Error: %s', $exception->getMessage())
+            );
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 declare(strict_types=1);
 
+use App\Helpers\App;
 use App\Helpers\Config;
 
 require_once __DIR__ . '\vendor\autoload.php';
 
-$config = Config::get('app', 'app_name');
-var_dump($config);
+$application = new App();
+echo $application->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
+echo $application->getLogPath() . PHP_EOL;
+echo $application->getEnvironment() . PHP_EOL;
+echo $application->isDebugMode() . PHP_EOL;
+
+if ($application->isRunningFromConsole()) {
+    echo 'From Console';
+} else {
+    echo 'From Browser';
+}
