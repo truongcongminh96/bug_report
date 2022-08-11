ac378355bab78d246fee7fbf4aaec94c6eba397f
s3minhtruong <minh.truong@s3corp.com.vn>
2022-08-11 09:38:25 +0700

    Creating a Database Query Builder Factory

---
+++ Src/Helpers/DbQueryBuilderFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Helpers;
+
+
+use App\Database\MySQLiConnection;
+use App\Database\MySQLiQueryBuilder;
+use App\Database\PDOQueryBuilder;
+use App\Database\QueryBuilder;
+use App\Exception\DatabaseConnectionException;
+
+class DbQueryBuilderFactory
+{
+    public static function make(
+        string $credentialsFile = 'database',
+        string $connectionType = 'pdo',
+        array $options = []
+    ): QueryBuilder
+    {
+        $connection = null;
+        $credentials = array_merge(Config::get($credentialsFile, $connectionType), $options)
+
+        switch ($connectionType) {
+            case 'pdo':
+                $connection = (new PDOConnection($credentials))->connect();
+                return new PDOQueryBuilder($connection);
+                break;
+            case 'mysqli':
+                $connection = (new MySQLiConnection($credentials))->connect();
+                return new MySQLiQueryBuilder($connection);
+                break;
+            default:
+                throw new DatabaseConnectionException(
+                    "Connection type is not recognize internally",
+                    ['type' => $connectionType]
+                );
+        }
+    }
+}
